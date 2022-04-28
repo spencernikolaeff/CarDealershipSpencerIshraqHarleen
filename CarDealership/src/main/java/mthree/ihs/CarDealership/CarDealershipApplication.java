056f29d03d962a38808452a5649e44001d0152c2
SpencerNikolaeff <spni5258@colorado.edu>
2022-04-28 13:22:19 -0600

    added basic file setup

---
+++ CarDealership/src/main/java/mthree/ihs/CarDealership/CarDealershipApplication.java
@@ -0,0 +1,13 @@
+package mthree.ihs.CarDealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipApplication.class, args);
+	}
+
+}
