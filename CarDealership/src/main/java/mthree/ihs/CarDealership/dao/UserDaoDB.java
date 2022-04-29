315407005f8a7d1572b64fb8bf716f1193ba8ff5
Harley Cheema <harleenscheema@gmail.com>
2022-04-29 14:43:10 -0500

    commit

--- CarDealership/src/main/java/mthree/ihs/CarDealership/dao/UserDaoDB.java
+++ CarDealership/src/main/java/mthree/ihs/CarDealership/dao/UserDaoDB.java
@@ -4,10 +4,45 @@
  */
 package mthree.ihs.CarDealership.dao;
 
+import java.util.List;
+import mthree.ihs.CarDealership.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author harle
  */
-public class UserDaoDB {
-    
+@Repository
+public class UserDaoDB implements UserDao {
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @Override
+    public User getUserById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User addUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void editUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deleteUserById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
