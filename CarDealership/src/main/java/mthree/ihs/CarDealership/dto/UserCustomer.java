ee08e8edcc85772c9f2857feb40afc8d9acb894c
SpencerNikolaeff <spni5258@colorado.edu>
2022-04-29 10:17:35 -0600

    user dtos

---
+++ CarDealership/src/main/java/mthree/ihs/CarDealership/dto/UserCustomer.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mthree.ihs.CarDealership.dto;
+
+/**
+ *
+ * @author 17202
+ */
+public class UserCustomer extends User {
+    //attributes
+    private String phoneNumber;
+    private String street1;
+    private String street2;
+    private String city;
+    private String state;
+    private int zipcode;
+
+    //empty constructor
+    public UserCustomer() {
+    }
+
+    //getter and setters
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public String getStreet2() {
+        return street2;
+    }
+
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+    
+    
+    
+}
