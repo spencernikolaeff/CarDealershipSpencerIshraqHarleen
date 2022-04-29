b778fcf5ea5c6876777e17c22e8304742d017ab3
SpencerNikolaeff <spni5258@colorado.edu>
2022-04-29 12:28:57 -0600

    added special dao, edited special dto

---
+++ CarDealership/src/main/java/mthree/ihs/CarDealership/dao/SpecialDao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mthree.ihs.CarDealership.dao;
+
+import java.util.List;
+import mthree.ihs.CarDealership.dto.Special;
+
+/**
+ *
+ * @author Spencer
+ */
+public interface SpecialDao {
+    
+    //basic methods
+    Special getSpecialById(int id);
+    List<Special> getAllSpecials();
+    Special addSpecial(Special special);
+    void updateSpecial(Special special);
+    void deleteSpecial(int id);
+    
+}
