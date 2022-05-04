32eba17323931e1d9cf7eebd12d00458461cd9be
Harley Cheema <harleenscheema@gmail.com>
2022-05-04 10:41:01 -0500

    new commit

---
+++ CarDealership/src/main/resources/static/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    alert("BIG HUGE SALE!!!");
+	hondaCivic();
+});
+
+function hondaCivic() {
+    $('#hondaCivicButton').click(function(){
+		alert("Try again next week :/");
+	});
+}
