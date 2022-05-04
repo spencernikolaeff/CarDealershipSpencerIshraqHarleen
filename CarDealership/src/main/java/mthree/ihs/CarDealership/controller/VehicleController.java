4b9b6872ff76cb5d5204724c9e396d6939be726c
SpencerNikolaeff <spni5258@colorado.edu>
2022-05-04 10:11:31 -0600

    idk

--- CarDealership/src/main/java/mthree/ihs/CarDealership/controller/VehicleController.java
+++ CarDealership/src/main/java/mthree/ihs/CarDealership/controller/VehicleController.java
@@ -5,12 +5,14 @@
 package mthree.ihs.CarDealership.controller;
 
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import mthree.ihs.CarDealership.dto.Vehicle;
 import mthree.ihs.CarDealership.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -26,21 +28,27 @@ public class VehicleController {
     String year = "1994";
     String make = "A4";
     String vmodel = "Audi";
+    String body = "";
+    String interior = "";
+    String exterior = "";
     
-    @GetMapping("/#")
-    public String loadFeatured(Model model) {
-        model.addAttribute("featured1year", year);
-        model.addAttribute("featured1model", vmodel);
-        model.addAttribute("featured1make", make);
-        return "/#";
-    }
+//    @GetMapping("/index.html")
+//    public String loadFeatured(Model model) {
+//        model.addAttribute("featured1year", year);
+//        model.addAttribute("featured1model", vmodel);
+//        model.addAttribute("featured1make", make);
+//        return "/index.html";
+//    }
     
     @GetMapping("inventory/new.html")
     public String loadNewInventory(Model model) {
         //model.addAttribute("featured1iconurl", temp.getIconURL());
         model.addAttribute("year", year);
-        model.addAttribute("model", model);
+        model.addAttribute("model", vmodel);
         model.addAttribute("make", make);
+        model.addAttribute("body", body);
+        model.addAttribute("interior", interior);
+        model.addAttribute("exterior", exterior);
         //model.addAttribute("featured1price", temp.getPrice());
         return "inventory/new.html";
     }
@@ -49,11 +57,30 @@ public class VehicleController {
     public String loadUsedInventory(Model model) {
         //model.addAttribute("featured1iconurl", temp.getIconURL());
         model.addAttribute("year", year);
-        model.addAttribute("model", model);
+        model.addAttribute("model", vmodel);
         model.addAttribute("make", make);
+        model.addAttribute("body", body);
+        model.addAttribute("interior", interior);
+        model.addAttribute("exterior", exterior);
         //model.addAttribute("featured1price", temp.getPrice());
         return "inventory/used.html";
     }
+    
+    @PostMapping("searchForm")
+    public String searchNewVehicles(HttpServletRequest request) {
+        String[] inputs = {"",""};
+        inputs[0] = request.getParameter("keyword");
+        inputs[1] = "0";
+        Vehicle test = service.searchVehicle(inputs);
+        body = test.getBody();
+        make = test.getMake();
+        vmodel = test.getModel();
+        interior = test.getInteriorColor();
+        exterior = test.getExteriorColor();
+        return "redirect:/inventory/new.html";
+    }
+    
+    
 //    @GetMapping("index.html")
 //    public String putVehicleInFeatured(Model model) {
 ////        Vehicle temp = service.getVehicleById(1);
